69dc453056bcaa46fcf66b7ea80a3502fe28bcdd
DENNKA <46068587+DENNKA@users.noreply.github.com>
2020-01-27 15:05:35 +0400

    Update KeysProtection.ino

--- KeysProtection.ino
+++ KeysProtection.ino
@@ -1,6 +1,23 @@
+#include <SPI.h>
+#include <MFRC522.h> // библиотека "RFID".
+#define SS_PIN 10
+#define RST_PIN 9
 
-#define KEYS_SIZE 1
-#define PIN_CARD 7
+#define DEBUG 
+
+#ifdef DEBUG
+  #define d(x) Serial.print(#x); Serial.print(" = "); Serial.println(x, DEC);
+  #define m(x) Serial.println(x);
+#else
+  #define d(x) ;
+  #define m(x) ;
+#endif
+
+#define KEYS_SIZE 2
+
+#define TIMER_PIN 300
+#define TIMER_CARD 10000
+unsigned long timerStart;
 
 struct Key{
   int room;
@@ -10,46 +27,99 @@ struct Key{
 
 Key keys[KEYS_SIZE];
 
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
+
 void setup() {
   Serial.begin(9600);
-  keys[0].room = 100;
-  keys[0].pin = 8;
-
+  int i = 0;
+  #define addKey(room1, pin1) keys[i].room = room1; keys[i].pin = pin1; i++;
+  addKey(108, 8)
+  addKey(107, 7)
 
   for (auto &i : keys){
     i.isKey = 1;
     pinMode(i.pin, INPUT);
   }
-  pinMode(PIN_CARD, INPUT);
+  Serial.println("Waiting for card...");
+  SPI.begin();  //  инициализация SPI
+  mfrc522.PCD_Init();     // инициализация MFRC522
 }
 
-#define d(x) Serial.print("#x "); Serial.print(x, DEC");
-#define dd d(isCard) d(pinValue) d(keys[i].isKey)
+bool isCard = 0;
 
-void loop() {
-  delay(200);
-  bool isCard = 0;
+void alert(){
+  m("ALERT!!!")
+}
+
+void scanCard(){
+  if (isCard == false && mfrc522.PICC_IsNewCardPresent()){
+    if (mfrc522.PICC_ReadCardSerial()) {
+      m("карта считана")
+      isCard = true;
+      timerStart = millis();
+
+      // Выдача серийного номера метки.
+      for (byte i = 0; i < mfrc522.uid.size; i++)
+      {
+        uidDecTemp = mfrc522.uid.uidByte[i];
+        uidDec = uidDec * 256 + uidDecTemp;
+      }
+      m("card UID: ")
+      m(uidDec)
+    }
+  }
+}
+
+void resetCard(){
+ isCard = false;
+ uidDec = 0;
+}
+
+void loop(){
+  m(" ")
+  
+  if (isCard == true && timerStart + TIMER_CARD < millis()){
+    isCard = false;
+    uidDec = 0;
+  }
+  
+  scanCard();
+  d(isCard)
+  d(uidDec)
+  delay(600);
+  
   for (int i = 0; i < KEYS_SIZE; i++){
     byte pinValue;
     pinValue = digitalRead(keys[i].pin);
-    isCard = digitalRead(PIN_CARD);
+    if(pinValue != keys[i].isKey){
+      delay(TIMER_PIN);
+      pinValue = digitalRead(keys[i].pin);
+    }
     
     if(isCard == false && pinValue == LOW && keys[i].isKey == true){
-      
-      while (isCard == false){
-      //ALERT
-      isCard = digitalRead(PIN_CARD);
-      Serial.println("ALERT!!!");
+      while (pinValue == LOW){
+        pinValue = digitalRead(keys[i].pin);
+        if(pinValue == HIGH){
+            delay(TIMER_PIN);
+            pinValue = digitalRead(keys[i].pin);
+        }
+        scanCard();
+        if (isCard == true) return;
+        alert();
       }
-      keys[i].isKey = false;
+      resetCard();
     }
     else if (isCard == true && keys[i].isKey == true && pinValue == LOW){
       Serial.println("Key get out");
+      Serial.println(keys[i].room);
       keys[i].isKey = false;
-      }
-       if (keys[i].isKey == false && pinValue == HIGH){
-        Serial.println("Key get up");
-        keys[i].isKey = true;
-        }
+      resetCard();
+    }
+    if (keys[i].isKey == false && pinValue == HIGH){
+      Serial.println("Key get up");
+      Serial.println(keys[i].room);
+      keys[i].isKey = true;
+    }
   }
 }
